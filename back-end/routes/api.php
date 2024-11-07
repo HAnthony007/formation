bc2580c2c437ccc0a99e6412cacf7f9654ba613c
theLastAirBender <fmihaja041@gmail.com>
2024-11-07 23:14:06 +0100

    login fait

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
 
-Route::post('/register',[AuthController::class,'register']);
+// Route::post('/register',[AuthController::class,'register']);
 
 Route::middleware('auth:api')->group(function(){
     Route::get('/listeUser',[User::class,'allUser']);
