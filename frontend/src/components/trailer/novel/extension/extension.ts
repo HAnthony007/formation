221b4e7f00799a9581b17184b752e397abe0f856
HAnthony007 <harifetraanthonyr@gmail.com>
2024-11-18 14:27:57 +0300

    Update extension novel

--- frontend/src/components/trailer/novel/extension/extension.ts
+++ frontend/src/components/trailer/novel/extension/extension.ts
@@ -1,5 +1,5 @@
 import { cx } from 'class-variance-authority'
-import { AIHighlight, Placeholder, TiptapImage, TiptapLink } from 'novel/extensions'
+import { AIHighlight, HorizontalRule, Placeholder, StarterKit, TaskItem, TaskList, TiptapImage, TiptapLink, UpdatedImage } from 'novel/extensions'
 import { UploadImagesPlugin } from 'novel/plugins'
 
 const aiHilight = AIHighlight
@@ -22,4 +22,108 @@ const tiptapImage = TiptapImage.extend({
             })
         ]
     },
+}).configure({
+    allowBase64: true,
+    HTMLAttributes: {
+        class: cx(
+            "rounded-lg border border-muted"
+        )
+    }
 })
+
+const updateImage = UpdatedImage.configure({
+    HTMLAttributes:{
+        class: cx(
+            "rounded-lg border border-muted"
+        )
+    }
+})
+
+const tastList = TaskList.configure({
+    HTMLAttributes: {
+        class: cx(
+            "not-prose pl-2"
+        )
+    }
+})
+
+const taskItem = TaskItem.configure({
+    HTMLAttributes: {
+        class: cx(
+            "flex gap-2 items-start my-4"
+        )
+    },
+    nested: true
+})
+
+const horizontalRule = HorizontalRule.configure({
+    HTMLAttributes: {
+        class: cx(
+            "mt-4 mb-6 border-t border-muted-foreground"
+        )
+    }
+})
+
+const starterKit = StarterKit.configure({
+    bulletList: {
+        HTMLAttributes: {
+            class: cx(
+                "list-disc list-outside leading-3 -mt-2"
+            )
+        }
+    },
+    orderedList: {
+        HTMLAttributes: {
+            class: cx(
+                "list-decimal list-outside leading-3 -mt-2"
+            )
+        }
+    },
+    listItem: {
+        HTMLAttributes: {
+            class: cx(
+                "leading-normal -mb-2"
+            )
+        }
+    },
+    blockquote: {
+        HTMLAttributes: {
+            class: cx(
+                "border-l-4 border-primary"
+            )
+        }
+    },
+    codeBlock: {
+        HTMLAttributes: {
+            class: cx(
+                "rounded-md bg-muted text-muted-foreground border p-5 font-mono font-medium"
+            )
+        }
+    },
+    code: {
+        HTMLAttributes: {
+            class: cx(
+                "rounded-md bg-muted  px-1.5 py-1 font-mono font-medium"
+            ),
+            spellcheck: "false",
+        }
+    },
+    horizontalRule: false,
+    dropcursor: {
+        color: "#DBEAFE",
+        width: 4,
+    },
+    gapcursor: false
+});
+
+export const defaultExtensions = [
+    starterKit,
+    tiptapLink,
+    tiptapImage,
+    updateImage,
+    horizontalRule,
+    tastList,
+    taskItem,
+    aiHilight,
+    placeholder
+]
